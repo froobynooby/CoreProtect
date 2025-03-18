1384c88453836f082e86e6acf301531fe120f651
froobynooby <froobynooby@froobworld.com>
2025-03-18 20:43:59 +0930

    Fix rollback of dyed bundles

--- src/main/java/net/coreprotect/bukkit/Bukkit_v1_17.java
+++ src/main/java/net/coreprotect/bukkit/Bukkit_v1_17.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Material;
+import org.bukkit.Tag;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -154,7 +155,7 @@ public class Bukkit_v1_17 extends BukkitAdapter {
 
     @Override
     public boolean setItemMeta(Material rowType, ItemStack itemstack, List<Map<String, Object>> map) {
-        if (rowType == Material.BUNDLE) {
+        if ((Tag.ITEMS_BUNDLES.isTagged(rowType))) {
             BundleMeta meta = (BundleMeta) itemstack.getItemMeta();
             for (Map<String, Object> itemData : map) {
                 ItemStack itemStack = ItemUtils.unserializeItemStack(itemData);
